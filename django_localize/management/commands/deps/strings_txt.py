f569a58beb8a438f80fbf4115d8bb2ef3226f1f1
Timofey <t.danshin@corp.mail.ru>
2018-02-20 19:59:46 +0300

    Added the files from the localization thing in Localads

---
+++ django_localize/management/commands/deps/strings_txt.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+# coding: utf-8
+from __future__ import print_function
+from collections import namedtuple, defaultdict
+import re
+
+TransAndKey = namedtuple("TransAndKey", "translation, key")
+
+TRANSLATION = re.compile(r"(.*)\s*=\s*.*$", re.S | re.MULTILINE)
+MANY_DOTS = re.compile(r"\.{4,}")
+SPACE_PUNCTUATION = re.compile(r"\s[.,?!:;]")
+PLACEHOLDERS = re.compile(r"(%\(\d*\w+\)[ds])")
+
+LANG_ORDER = ["en", "en-GB", "ru"]
+
+SIMILARITY_THRESHOLD = 20.0 #%
+
+
+class StringsTxt:
+
+    def __init__(self, strings_path):
+        self.strings_path = strings_path
+        self.translations = defaultdict(lambda: defaultdict(str)) # dict<key, dict<lang, translation>>
+        self.translations_by_language = defaultdict(dict) # dict<lang, dict<key, translation>>
+        self.comments_and_tags = defaultdict(dict)
+        self.all_langs = set()
+        self.keys_in_order = []
+        self.warnings = []
+
+        self._read_file()
+        self._populate_translations_by_langs()
+
+
+    def _read_file(self):
+        with open(self.strings_path) as strings:
+            for line in strings:
+                line = line.strip()
+                if not line:
+                    continue
+                if line.startswith("[["):
+                    self.keys_in_order.append(line)
+                    continue
+                if line.startswith("["):
+                    # if line in self.translations:
+                    #     print("Duplicate key {}".format(line))
+                    #     continue
+                    self.translations[line] = {}
+                    current_key = line
+                    if current_key not in self.keys_in_order:
+                        self.keys_in_order.append(current_key)
+                    continue
+
+                if TRANSLATION.match(line):
+                    lang, tran = self._parse_lang_and_translation(line)
+
+                    if lang == "comment" or lang == "tags":
+                        self.comments_and_tags[current_key][lang] = tran
+                        continue
+
+                    self.translations[current_key][lang] = tran
+                    self.all_langs.add(lang)
+
+
+    def _parse_lang_and_translation(self, line):
+        ret = tuple(map(self._process_string, line.split(" = ", 1)))
+        assert len(ret) == 2, "Couldn't parse the line: {0}".format(line)
+        return ret
+
+
+    def _process_string(self, string):
+        if MANY_DOTS.search(string):
+            print("WARNING: 4 or more dots in the string: {0}".format(string))
+        return str.strip(string).replace("...", "…")
+
+
+    def _populate_translations_by_langs(self):
+        for lang in self.all_langs:
+            trans_for_lang = {}
+            for key, tran in self.translations.items(): # (tran = dict<lang, translation>)
+                if lang not in tran:
+                    continue
+                trans_for_lang[key] = tran[lang]
+            self.translations_by_language[lang] = trans_for_lang
+
+
+    def write_formatted(self, target_file=None, languages=None):
+        before_block = ""
+        if target_file is None:
+            target_file = self.strings_path
+        non_itunes_langs = sorted(list(self.all_langs - set(LANG_ORDER)))
+        with open(target_file, "w") as outfile:
+            for key in self.keys_in_order:
+                if not key:
+                    continue
+                if key in self.translations:
+                    tran = self.translations[key]
+                else:
+                    if key.startswith("[["):
+                        outfile.write("{0}{1}\n".format(before_block, key))
+                        before_block = "\n"
+                    continue
+
+                outfile.write("{0}  {1}\n".format(before_block, key))
+                before_block = "\n"
+
+                if key in self.comments_and_tags:
+                    for k, v in self.comments_and_tags[key].items():
+                        outfile.write("    {0} = {1}\n".format(k, v))
+                self._write_translations_for_langs(LANG_ORDER, tran, outfile, only_langs=languages)
+                self._write_translations_for_langs(non_itunes_langs, tran, outfile, only_langs=languages)
+
+
+    def _write_translations_for_langs(self, langs, tran, outfile, only_langs=None):
+        langs_to_write = []
+
+        if only_langs:
+            for lang in only_langs:
+                if lang in langs:
+                    langs_to_write.append(lang)
+        else:
+            langs_to_write = langs
+
+        for lang in langs_to_write:
+            if lang in tran:
+                outfile.write("    {0} = {1}\n".format(lang, tran[lang]))
+
+
+    def _has_space_before_punctuation(self, lang_and_string):
+        lang, string = lang_and_string
+        if lang == "fr":
+            return False
+        if SPACE_PUNCTUATION.search(string):
+            return True
+        return False
+
+
+    def find_spaces_before_punctuation(self):
+        loc_warnings = []
+
+        for key, lang_and_trans in self.translations.items():
+            with_spaces = list(filter(
+                self._has_space_before_punctuation, lang_and_trans.items()))
+
+            if with_spaces:
+                loc_warnings.append("{}:\n{}\n".format(
+                    key,
+                    "\n".join(map(lambda tup: "{}: {}".format(*tup), with_spaces))
+                ))
+        if loc_warnings:
+            self.warnings.append(
+                "These strings have spaces before punctuation marks:\n{}".format(
+                    "\n".join(loc_warnings)
+                )
+            )
+
+
+    def _check_placeholders_in_block(self, block_key):
+        wrong_placeholders_strings = []
+        key_placeholders = sorted(PLACEHOLDERS.findall(block_key))
+        for lang, translation in self.translations[block_key].items():
+            found = sorted(PLACEHOLDERS.findall(translation))
+            if not key_placeholders == found: # must be sorted
+                wrong_placeholders_strings.append("{} : {}".format(lang, translation))
+
+        return wrong_placeholders_strings
+
+
+    def find_wrong_paceholders(self):
+        warnings = []
+
+        for key, lang_and_trans in self.translations.items():
+            for wr_placeholders in self._check_placeholders_in_block(key):
+                warnings.append("{}\nEn: {}\n{}".format(
+                    key, lang_and_trans["en"], wr_placeholders
+                ))
+
+        if warnings:
+            self.warnings.append(
+                "These strings have wrong numbers of placeholders in them:\n{}".format(
+                    "\n".join(warnings)
+                )
+            )
---
+++ django_localize/management/commands/generate_localizations.py
@@ -0,0 +1,159 @@
+from os import path
+
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+
+from .deps.strings_txt import StringsTxt
+
+FORMAT_BY_TAG = {
+    "comment": "# {}\n",
+    "tags": "# tags: {}\n",
+}
+
+
+class Command(BaseCommand):
+    help = "Generate .po files from the strings.txt file"
+
+    def __init__(self):
+        super(BaseCommand, self).__init__()
+        self.all_langs = None
+        self.strings_txt = None
+        self.warnings = []
+        self.locale_dir = None
+
+
+    def add_arguments(self, parser):
+        my_path = path.realpath(__file__)
+        self.locale_dir = path.abspath(
+            path.join(path.dirname(my_path), "..", "..", "locale")
+        )
+
+
+    def path_for_lang(self, lang):
+        return path.join(self.locale_dir, lang, "LC_MESSAGES")
+
+
+    def strip_key(self, key):
+        return key.strip("[]")
+
+
+    def handle(self, *args, **kwargs):
+        strings_path = path.join(self.locale_dir, "strings.txt")
+        countries_names = path.join(self.locale_dir, "countries_names.txt")
+        self.strings_txt = [StringsTxt(strings_path), StringsTxt(countries_names)]
+        self.all_langs = set()
+        for st in self.strings_txt:
+            self.all_langs.update(st.all_langs)
+
+
+        self.create_locale_folders()
+
+        self.write_po_files()
+        for strings_txt in self.strings_txt:
+            strings_txt.write_formatted()
+            strings_txt.find_spaces_before_punctuation()
+            strings_txt.find_wrong_paceholders()
+
+            self.warnings.extend(strings_txt.warnings)
+
+        for warning in self.warnings:
+            print("WARNING: {}\n\n".format(warning))
+
+        call_command('compilemessages')
+
+
+    def write_po_files(self):
+        for lang in self.all_langs:
+            self.write_one_po_file(lang)
+
+
+    def write_comment_and_tag(self, str_txt, key, outfile):
+        for tag, text in str_txt.comments_and_tags.get(key, {}).items():
+            outfile.write(FORMAT_BY_TAG[tag].format(text))
+
+
+    def write_one_po_file(self, lang):
+        if not path.exists(self.path_for_lang(lang)):
+            return
+
+        with open(path.join(self.path_for_lang(lang), "django.po"), "w") as outfile:
+            outfile.write(PoFileHeader.get_header_for_lang(lang))
+            for str_txt in self.strings_txt:
+                translations = str_txt.translations_by_language[lang]
+                for key in str_txt.keys_in_order:
+                    if key.startswith("[["):
+                        outfile.write('####\n# {}\n####\n\n'.format(self.strip_key(key)))
+                        continue
+                    self.write_comment_and_tag(str_txt, key, outfile)
+                    if key in translations:
+                        outfile.write("msgid \"{}\"\n".format(self.strip_key(key).replace('"', r'\"')))
+                        outfile.write("msgstr \"{}\"\n\n".format(translations[key].replace('"', r'\"')))
+
+
+    def create_locale_folders(self):
+        paths = map(self.path_for_lang, self.all_langs)
+        for fpath, lang in zip(paths, self.all_langs):
+            if not path.exists(fpath):
+                self.warnings.append(
+"""No localization directory exists for language: {lang}. Please, run
+
+        python manage.py makemessages -l {lang}
+
+to create the corresponding folder structure. Please check if there are any special
+headers added in the generated .po file and add them to this script.\n
+***  FOR NOW THIS LANGUAGE ({lang}) WILL BE IGNORED  ***""".format(lang=lang)
+                )
+                # makedirs(fpath)
+
+
+class PoFileHeader:
+    @staticmethod
+    def get_header_for_lang(lang):
+        header = '''# AUTOMATICALLY GENERATED FROM STRINGS.TXT
+#
+# DO NOT MODIFY THIS FILE MANUALLY!
+# To modify translations, add them to the strings.txt file and commit them to git.
+# run `
+#    python3 manage.py generate_localizations
+# `
+# And then commit those changes to git.
+# To make it clear, the modifications to strings.txt should be committed
+# separately, and the modifications to the generated files should also be
+# committed separately
+#
+# , fuzzy
+msgid
+""
+msgstr
+""
+"Project-Id-Version: PACKAGE VERSION\\n"
+"Report-Msgid-Bugs-To: dtv@maps.me\\n"
+"POT-Creation-Date: \\n"
+"PO-Revision-Date: \\n"
+"Last-Translator: \\n"
+"Language-Team: \\n"
+"Language: {lang}\\n"
+"MIME-Version: 1.0\\n"
+"Content-Type: text/plain; charset=UTF-8\\n"
+"Content-Transfer-Encoding: 8bit\\n"
+\n'''.format(lang=lang)
+
+        # Plural forms headers for different languages.
+        header += {
+            'ru': '''"Plural-Forms: nplurals=4; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n"
+"%10<=4 && (n%100<12 || n%100>14) ? 1 : n%10==0 || (n%10>=5 && n%10<=9) || (n"
+"%100>=11 && n%100<=14)? 2 : 3);\\n"
+\n''',
+            'pl': '''"Plural-Forms: nplurals=3; plural=(n==1 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 "
+"|| n%100>=20) ? 1 : 2);\\n"\n''',
+
+            'es': '"Plural-Forms: nplurals=2; plural=(n != 1);\\n"\n',
+            'de': '"Plural-Forms: nplurals=2; plural=(n != 1);\\n"\n',
+            'it': '"Plural-Forms: nplurals=2; plural=(n != 1);\\n"\n',
+            'nl': '"Plural-Forms: nplurals=2; plural=(n != 1);\\n"\n',
+
+            'fr': '"Plural-Forms: nplurals=2; plural=(n > 1);\\n"\n',
+            'tr': '"Plural-Forms: nplurals=2; plural=(n > 1);\\n"\n',
+
+        }.get(lang, '"Plural-Forms: nplurals=2; plural=(n != 1);\\n"\n')
+        return header
